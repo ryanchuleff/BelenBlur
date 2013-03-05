87f6eef03fdeefc61c10214ee4727c92d9a63404
Ryan Yanchuleff <ryan@yanchuleff.com>
2013-03-04 22:16:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BelenBlur::Application.config.secret_token = '7e56485d63f143af8554784d52739d3eb78a5fd6522b54c5e23e32d58cbd80a1e3413b98afca9309b412815b72a25199ca2a682876c869206171bd73b789e083'
